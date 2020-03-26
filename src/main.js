ad6d7a41103cba9c2a2446a0648e1eaa44518841
OKAUEND <def1f23sbb@gmail.com>
2020-03-26 17:17:51 +0900

    ADD - グラフの縦軸の表示で、KやM表示など略表示を無くすために、mainでOptionを設定

--- src/main.js
+++ src/main.js
@@ -5,12 +5,19 @@ import router from "./router";
 import store from "./store";
 import axios from "axios";
 import VueAxions from "vue-axios";
+import Highcharts from "highcharts";
 import HighchartsVue from "highcharts-vue";
 
 Vue.config.productionTip = false;
 
 Vue.use(VueAxions, axios);
 Vue.use(HighchartsVue);
+
+Highcharts.setOptions({
+  lang: {
+    numericSymbols: null
+  }
+});
 
 new Vue({
   router,
