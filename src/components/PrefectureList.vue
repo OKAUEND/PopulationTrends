1069f5833a7d4333305b5886d85fb0176f832b34
OKAUEND <def1f23sbb@gmail.com>
2020-03-26 10:06:37 +0900

    NEW  - 都道府県のリストを表示するコンポーネントとテストファイルを作成

---
+++ src/components/PrefectureList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="PrefectureList"></div>
+</template>
+
+<script>
+export default {
+  name: "PrefectureList",
+  data() {
+    return {
+      prefectures: []
+    };
+  },
+  mounted() {}
+};
+</script>
+
+<style scoped lang="scss"></style>
