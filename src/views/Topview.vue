2f3a2c086df6deea25cffb8897224ed67fba69ec
OKAUEND <def1f23sbb@gmail.com>
2020-03-27 21:12:02 +0900

    CHANGE - ファイル名がHomeだとなにのファイルかわからないため、一番上のであるのを表すのに変更

--- src/views/Topview.vue
+++ src/views/Topview.vue
@@ -1,15 +1,16 @@
 <template>
-  <div class="Home">
+  <section class="Topview">
+    <h1 class="Topview__Title">都道府県別人口推移</h1>
     <prefecture-list v-model="setPrefectures" />
     <populationschart :prefectures="prefectures" />
-  </div>
+  </section>
 </template>
 
 <script>
 import PrefectureList from "@/components/PrefectureList.vue";
 import populationschart from "@/components/populationschart.vue";
 export default {
-  name: "Home",
+  name: "Topview",
   components: {
     PrefectureList,
     populationschart
@@ -33,11 +34,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.Home {
+.Topview {
   width: 100%;
   @media screen and (min-width: 761px) {
     width: 70%;
     margin: 0 auto;
+  }
+
+  &__Title {
+    height: 50px;
+    text-align: center;
+    line-height: 50px;
   }
 }
 </style>
