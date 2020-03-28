5b95c4e1544ac578c949795fff8458e16f9d442a
OKAUEND <def1f23sbb@gmail.com>
2020-03-28 20:48:21 +0900

    NEW - エラー発生時に遷移をさせるためのコンポーネントを作成

---
+++ src/components/ErrorPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1></h1>
+    <p></p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "ErrorPage"
+};
+</script>
+
+<style lang="scss" scoped></style>
