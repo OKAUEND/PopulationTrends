e8abb48246093da085d4378cfbe62644c5341a74
OKAUEND <def1f23sbb@gmail.com>
2020-03-29 10:13:53 +0900

    ADD - エラーメッセージはハードコーディングをやめて、外部管理にし変更しやすいように また、if文だったのをSwitch文にし、見やすいように変更

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import message from "@/assets/message.json";
 
 Vue.use(Vuex);
 
@@ -19,26 +20,28 @@ export default new Vuex.Store({
   },
   getters: {
     getErrorStatus: state => {
-      if (state.ErrorStatus === 403) {
-        return `${state.ErrorStatus} Forbidden`;
-      } else if (state.ErrorStatus === 404) {
-        return `${state.ErrorStatus} Not Found`;
-      } else if (state.ErrorStatus === 429) {
-        return `${state.ErrorStatus} Too Many Requests`;
-      } else {
-        //5xxエラーはコレを表示する
-        return `5xx Server Error`;
+      switch (state.ErrorStatus) {
+        case 403:
+          return `${state.ErrorStatus} Forbidden`;
+        case 404:
+          return `${state.ErrorStatus} Not Found`;
+        case 429:
+          return `${state.ErrorStatus} Too Many Requests`;
+        default:
+          //5xxエラーはコレを表示する
+          return `5xx Server Error`;
       }
     },
     getErrorMessage: state => {
       console.log(state);
-      if (state.ErrorStatus === 403 || state.ErrorStatus === 404) {
-        return "お探しのページは見つかりませんでした。";
-      } else if (state.ErrorStatus === 429) {
-        return "アクセス制限回数に達しました。時間おき再度アクセスしてください。";
-      } else {
-        //5xxエラーはコレを表示する
-        return "サーバーへアクセス出来ませんでした。再度時間をおきからアクセスしてください。";
+      switch (state.ErrorStatus) {
+        case 403 || 404:
+          return message.Status403And404;
+        case 429:
+          return message.Status429;
+        default:
+          //5xxエラーはコレを表示する
+          return message.Status5xx;
       }
     }
   }
