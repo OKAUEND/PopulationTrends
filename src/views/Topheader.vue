f79af85f73e0726be169219bef0f69040199a26f
OKAUEND <def1f23sbb@gmail.com>
2020-04-01 16:07:40 +0900

    ADD - タイトルをクリックしたら、Topviewページへ遷移するようにrouterを追加

--- src/views/Topheader.vue
+++ src/views/Topheader.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="Header">
-    <h1 class="Header__Title">都道府県別人口推移</h1>
+    <router-link tag="div" to="/" class="Header__Container">
+      <h1 class="Header__Title">都道府県別人口推移</h1>
+    </router-link>
   </div>
 </template>
 
@@ -13,9 +15,15 @@ export default {
 <style lang="scss" scoped>
 .Header {
   height: 50px;
+
+  &__Container {
+    margin: 0 auto;
+    width: 300px;
+  }
   &__Title {
-    text-align: center;
     line-height: 50px;
+    cursor: pointer;
+    text-align: center;
   }
 }
 </style>
